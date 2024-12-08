103ef18d41ec64f78ac34019d1b5d6fb11f2f2d5
Tanveer Ahmed <tanveerahmed08.1999@gmail.com>
2024-12-08 20:11:30 +0500

    Home

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import './header.css'
 
 const Header = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <div className="container header-container">
+        <h5>Hello I'm</h5>
+        <h1>Tanveer Ahmed</h1>
+        <h5 className='text-light'>Front-End Developer</h5>
+      </div>
+    </header>
   )
 }
 
