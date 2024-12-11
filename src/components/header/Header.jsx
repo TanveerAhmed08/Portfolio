eb08f68768a0ba43b99245f174fa39091cf62997
Tanveer Ahmed <tanveerahmed08.1999@gmail.com>
2024-12-12 00:36:20 +0500

    Header work

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import Headersocial from './Headersocial'
 
 const Header = () => {
   return (
@@ -8,6 +11,14 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Tanveer Ahmed</h1>
         <h5 className='text-light'>Front-End Developer</h5>
+        <CTA />
+        <Headersocial />
+
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+
+        <a href="#contact" className='scroll-down'>Scroll Down</a>
       </div>
     </header>
   )
