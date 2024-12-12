45c77229e9daf9451bb418ea5f5bb04d62f5664d
Tanveer Ahmed <tanveerahmed08.1999@gmail.com>
2024-12-13 00:00:32 +0500

    nav and about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import { FaAward } from 'react-icons/fa'
+import { FiUser } from 'react-icons/fi'
+import { VscFolderLibrary } from 'react-icons/vsc'
+
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about-container">
+        <div className="about-me">
+          <div className="about-me-img">
+            <img src={ME} alt="About image" />
+          </div>
+        </div>
+        <div className="about-content">
+          <div className="about-cards">
+            <article className='about-card'>
+              <FaAward className='about-icon' />
+              <h5>Experience</h5>
+              <small>3+ Yeras </small>
+            </article>
+
+            <article className='about-card'>
+              <FiUser className='about-icon' />
+              <h5>Clients</h5>
+              <small>100+ </small>
+            </article>
+
+            <article className='about-card'>
+              <VscFolderLibrary className='about-icon' />
+              <h5>Projects</h5>
+              <small>80+ </small>
+            </article>
+          </div>
+          <p>I m Front end developer. I keep it concise, focusing on my relevant technical skills, project experience, and alignment with the job. Highlight a key project, connect your skills to the company's needs, and express genuine enthusiasm for the role.</p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
+    <section id='testimonial'>Testimonial</section>
   )
 }
 
