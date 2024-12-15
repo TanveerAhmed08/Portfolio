c8297f4df825557d0aa1339eca69f7df24e84294
Tanveer Ahmed <tanveerahmed08.1999@gmail.com>
2024-12-15 13:50:09 +0500

    footer

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './contact.css'
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
+
+
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact-container">
+        <div className="contact-options">
+          <article className="contact-option">
+            <MdOutlineEmail className='contact-option-icon' />
+            <h4>Email</h4>
+            <h5>tanveerahmed08.1999@gmail.com</h5>
+            <a href="mailto:tanveerahmed08.1999@gmail.com" target='_blank'>Send a message</a>
+          </article>
+          <article className="contact-option">
+            <RiMessengerLine className='contact-option-icon' />
+            <h4>Messenger</h4>
+            <h5>Tanveer Ahmed</h5>
+            <a href="https://m.me/profile.php?id=61561120005578" target='_blank'>Send a message</a>
+          </article>
+          <article className="contact-option">
+            <BsWhatsapp className='contact-option-icon' />
+            <h4>Whatsapp</h4>
+            <h5>+923053565751</h5>
+            <a href="https://api.whatsapp.com/send?phone+923053565751" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Emial' />
+          <textarea name="message" rows='7' placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import './footer.css'
+import { FaFacebookF } from 'react-icons/fa'
+import { FiInstagram } from 'react-icons/fi'
+import { IoLogoTwitter } from 'react-icons/io'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <a href="#" className='footer-logo'>Tanveer</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer-social">
+        <a href="https://facebook.com"><FaFacebookF /></a>
+        <a href="https://twitter.com"><IoLogoTwitter /></a>
+        <a href="https://instagram.com"><FiInstagram /></a>
+      </div>
+
+      <div className="footer-copyright">
+        <small>&copy; Tanveer - Portfolio. All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
